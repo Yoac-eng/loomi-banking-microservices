c36e822904e53e55ef5308c81f105737b252bf21
Cayo Cutrim <cayocutrim1991@gmail.com>
2026-01-10 20:59:37 -0300

    feat(clients): adding zod pipe, filter and controllers

--- apps/clients/src/controllers/clients.controller.ts
+++ apps/clients/src/controllers/clients.controller.ts
@@ -1,13 +1,75 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Patch,
+  Param,
+  Body,
+  UseInterceptors,
+  UploadedFile,
+  UsePipes,
+  ParseFilePipeBuilder,
+  HttpStatus,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
-import { ClientsService } from '../clients.service';
+import {
+  type UpdateUserDto,
+  updateUserSchema,
+} from '../application/dtos/update-user.dto';
+import { GetUserByIdUseCase } from '../application/useCases/get-user-by-id.use-case';
+import { UpdateProfilePictureUseCase } from '../application/useCases/update-profile-picture.use-case';
+import { UpdateUserUseCase } from '../application/useCases/update-user.use-case';
+import { ZodValidationPipe } from '../common/pipes/zod-validation.pipe';
 
-@Controller()
+@Controller('api/users')
 export class ClientsController {
-  constructor(private readonly clientsService: ClientsService) {}
+  constructor(
+    private readonly getUserByIdUseCase: GetUserByIdUseCase,
+    private readonly updateUserUseCase: UpdateUserUseCase,
+    private readonly updateProfilePictureUseCase: UpdateProfilePictureUseCase,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.clientsService.getHello();
+  @Get(':userId')
+  async getUserById(@Param('userId') userId: string) {
+    const user = await this.getUserByIdUseCase.execute(userId);
+    return user.toJson();
+  }
+
+  @Patch(':userId')
+  @UsePipes(new ZodValidationPipe(updateUserSchema))
+  async updateUser(
+    @Param('userId') userId: string,
+    @Body() data: UpdateUserDto,
+  ) {
+    const user = await this.updateUserUseCase.execute(userId, data);
+    return user.toJson();
+  }
+
+  @Patch(':userId/profile-picture')
+  @UseInterceptors(FileInterceptor('profilePicture'))
+  async updateProfilePicture(
+    @Param('userId') userId: string,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: /(jpg|jpeg|png)$/,
+        })
+        .addMaxSizeValidator({
+          maxSize: 5 * 1024 * 1024,
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+          fileIsRequired: true,
+        }),
+    )
+    file: Express.Multer.File,
+  ) {
+    const user = await this.updateProfilePictureUseCase.execute(
+      userId,
+      file.buffer,
+      file.mimetype,
+    );
+
+    return user.toJson();
   }
 }
--- apps/clients/src/main.ts
+++ apps/clients/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 
 import { ClientsModule } from './clients.module';
+import { ZodExceptionFilter } from './common/filters/zod-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(ClientsModule);
+  app.useGlobalFilters(new ZodExceptionFilter());
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
